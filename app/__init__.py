bd36eaac7986a234f048b93d8e037a55ba30b998
Manish Gupta <manish@dbsentry.com>
2020-08-26 20:42:38 -0500

    20200826 - Added License headers. Bug fixes

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,15 @@
+#############################################################################
+#                       Confidentiality Information                         #
+#                                                                           #
+# This module is the confidential and proprietary information of            #
+# DBSentry Corp.; it is not to be copied, reproduced, or transmitted in any #
+# form, by any means, in whole or in part, nor is it to be used for any     #
+# purpose other than that for which it is expressly provided without the    #
+# written permission of DBSentry Corp.                                      #
+#                                                                           #
+# Copyright (c) 2020-2021 DBSentry Corp.  All Rights Reserved.              #
+#                                                                           #
+#############################################################################
 ''' Keyper API app '''
 import logging
 import json
@@ -5,7 +17,7 @@ from os import environ
 from flask import Flask, jsonify
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
-from datetime import timedelta;
+from datetime import timedelta
 from .resources.errors import KeyperError
 from .utils.extensions import jwt, logs, ma
 from config import config
@@ -14,7 +26,7 @@ def create_app():
     ''' Create app '''
     app = Flask(__name__)
 
-    flask_config = "config." + config.get(environ.get('FLASK_CONFIG'), 'DevelopmentConfig')
+    flask_config = "config." + config.get(environ.get('FLASK_CONFIG'), 'ProductionConfig')
 
     #app.config.from_object("config.DevelopmentConfig")
 
