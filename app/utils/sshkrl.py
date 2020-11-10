ea56dfdb53606156dd531bf205027b62a6167762
Manish Gupta <manish@dbsentry.com>
2020-11-10 15:27:45 -0600

    Code Analysis

--- app/utils/sshkrl.py
+++ app/utils/sshkrl.py
@@ -21,22 +21,14 @@ Implements SSH KRL Lookup.
 
 class SSHKRL(object):
     ''' SSHKRL Class '''
-    ca_host_key = ''
-    ca_user_key = ''
     ca_krl_file = ''
-    ca_tmp_work_dir = ''
-    ca_tmp_work_delete_flag = True
     krl_buf_len = 0
     krl = {}
 
     def __init__(self):
         app.logger.debug("Enter")
         self.ca_dir = app.config["SSH_CA_DIR"]
-        self.ca_host_key = self.ca_dir + "/" + app.config["SSH_CA_HOST_KEY"]
-        self.ca_user_key = self.ca_dir + "/" + app.config["SSH_CA_USER_KEY"]
         self.ca_krl_file = self.ca_dir + "/" + app.config["SSH_CA_KRL_FILE"]
-        self.ca_tmp_work_dir = self.ca_dir + "/" + app.config["SSH_CA_TMP_WORK_DIR"]
-        self.ca_tmp_work_delete_flag = app.config["SSH_CA_TMP_DELETE_FLAG"]
 
         try:
             with open(self.ca_krl_file, mode="rb") as krl_file:
@@ -63,38 +55,15 @@ class SSHKRL(object):
                 self.krl["krl_flags"] = krlbuf[krl_buf_ptr:krl_buf_ptr+8]
                 krl_buf_ptr += 8
 
-                string_size = struct.unpack('>i', krlbuf[krl_buf_ptr:krl_buf_ptr+4])[0]
-                krl_buf_ptr += 4
+                krl_buf_ptr, reserved_string = self.read_string_from_buf(krlbuf, krl_buf_ptr)
+                krl_buf_ptr, self.krl["krl_comment"] = self.read_string_from_buf(krlbuf, krl_buf_ptr)
 
-                if (self.krl_buf_len < krl_buf_ptr + string_size):
-                    app.logger.error("KRL Parse Error at reserved string " + str(krl_buf_ptr))
-                    raise KeyperError(errors["KRLParseError"].get("msg"), errors["KRLParseError"].get("status"))
-
-                krl_buf_ptr += string_size
-                string_size = struct.unpack('>i', krlbuf[krl_buf_ptr:krl_buf_ptr+4])[0]
-                krl_buf_ptr += 4
-
-                if (self.krl_buf_len < krl_buf_ptr + string_size):
-                    app.logger.error("KRL Parse Error at comment " + str(krl_buf_ptr))
-                    raise KeyperError(errors["KRLParseError"].get("msg"), errors["KRLParseError"].get("status"))
-
-                if (string_size > 0):
-                    self.krl["krl_comment"] = krlbuf[krl_buf_ptr:krl_buf_ptr+string_size]
-                    krl_buf_ptr += string_size
-                
                 # Parse sections
                 while (krl_buf_ptr < self.krl_buf_len):
                     section_type = struct.unpack('c', krlbuf[krl_buf_ptr:krl_buf_ptr+1])[0]
                     krl_buf_ptr += 1
-                    string_size = struct.unpack('>i', krlbuf[krl_buf_ptr:krl_buf_ptr+4])[0]
-                    krl_buf_ptr += 4
 
-                    if (self.krl_buf_len < krl_buf_ptr + string_size):
-                        app.logger.error("KRL Parse Error at Sections " + str(krl_buf_ptr))
-                        raise KeyperError(errors["KRLParseError"].get("msg"), errors["KRLParseError"].get("status"))
-
-                    section_data = krlbuf[krl_buf_ptr:krl_buf_ptr+string_size]
-                    krl_buf_ptr += string_size
+                    krl_buf_ptr, section_data = self.read_string_from_buf(krlbuf, krl_buf_ptr)
 
                     if (section_type == b'\x01'):
                         section_ptr = 0
@@ -103,41 +72,16 @@ class SSHKRL(object):
                         if ("krl_certs" not in self.krl):
                             self.krl["krl_certs"] = []
                         while (section_ptr < section_data_len):
-                            string_size = struct.unpack('>i', section_data[section_ptr:section_ptr+4])[0]
-                            section_ptr += 4
-
-                            if (section_data_len < section_ptr + string_size):
-                                app.logger.error("KRL Parse Error at section 1. Section PTR: " + str(section_ptr))
-                                raise KeyperError(errors["KRLParseError"].get("msg"), errors["KRLParseError"].get("status"))
-
                             krl_certs = {}
-
-                            krl_certs["ca_key"] = section_data[section_ptr:section_ptr+string_size]
-                            section_ptr += string_size
-
-                            string_size = struct.unpack('>i', section_data[section_ptr:section_ptr+4])[0]
-                            section_ptr += 4
-
-                            if (section_data_len < section_ptr + string_size):
-                                app.logger.error("KRL Parse Error at section 1. Section PTR: " + str(section_ptr))
-                                raise KeyperError(errors["KRLParseError"].get("msg"), errors["KRLParseError"].get("status"))
-
-                            section_ptr += string_size
+                            section_ptr, krl_certs["ca_key"] = self.read_string_from_buf(section_data, section_ptr)
+                            section_ptr, reserved_string = self.read_string_from_buf(section_data, section_ptr)
 
                             cert_section_type = struct.unpack('c', section_data[section_ptr:section_ptr+1])[0]
                             section_ptr += 1
 
-                            string_size = struct.unpack('>i', section_data[section_ptr:section_ptr+4])[0]
-                            section_ptr += 4
-
-                            if (section_data_len < section_ptr + string_size):
-                                app.logger.error("KRL Parse Error at section 1. Section PTR: " + str(section_ptr))
-                                raise KeyperError(errors["KRLParseError"].get("msg"), errors["KRLParseError"].get("status"))
+                            section_ptr, cert_serial_list = self.read_string_from_buf(section_data, section_ptr)
 
                             if (cert_section_type == b'\x20'):
-                                cert_serial_list = section_data[section_ptr:section_ptr+string_size]
-                                section_ptr += string_size
-
                                 cert_serial_list_ptr = 0
                                 cert_serial_list_len = len(cert_serial_list)
 
@@ -147,8 +91,6 @@ class SSHKRL(object):
                                     app.logger.debug("Cert Serial No: " + str(struct.unpack('>q', cert_serial_list[cert_serial_list_ptr:cert_serial_list_ptr+8])[0]))
                                     cert_serial_list_ptr += 8
                                 app.logger.debug("Cert Serial List Size: " + str(len(krl_certs["cert_serial_list"])))
-                            else:
-                                section_ptr += string_size
 
                             self.krl["krl_certs"].append(krl_certs)
                     elif (section_type == b'\x02'):
@@ -156,16 +98,11 @@ class SSHKRL(object):
                         section_data_len = len(section_data)
                         if ("krl_keys" not in self.krl):
                             self.krl["krl_keys"] = []
+                        
                         while (section_ptr < section_data_len):
-                            string_size = struct.unpack('>i', section_data[section_ptr:section_ptr+4])[0]
-                            section_ptr += 4
+                            section_ptr, krl_key = self.read_string_from_buf(section_data, section_ptr)
+                            self.krl["krl_keys"].append(krl_key)
 
-                            if (section_data_len < section_ptr + string_size):
-                                app.logger.error("KRL Parse Error at section 2. Section PTR: " + str(section_ptr))
-                                raise KeyperError(errors["KRLParseError"].get("msg"), errors["KRLParseError"].get("status"))
-
-                            self.krl["krl_keys"].append(section_data[section_ptr:section_ptr+string_size])
-                            section_ptr += string_size
                         app.logger.debug("KRL Keys Size: " + str(len(self.krl["krl_keys"])))
                     elif (section_type == b'\x05'):
                         section_ptr = 0
@@ -173,22 +110,37 @@ class SSHKRL(object):
                         if ("krl_key_hash" not in self.krl):
                             self.krl["krl_key_hash"] = []
                         while (section_ptr < section_data_len):
-                            string_size = struct.unpack('>i', section_data[section_ptr:section_ptr+4])[0]
-                            section_ptr += 4
+                            section_ptr, krl_key_hash = self.read_string_from_buf(section_data, section_ptr)
+                            self.krl["krl_key_hash"].append(krl_key_hash)
+                            app.logger.debug("Key Hash: " + str(krl_key_hash))
 
-                            if (section_data_len < section_ptr + string_size):
-                                app.logger.error("KRL Parse Error at section 5. Section PTR: " + str(section_ptr))
-                                raise KeyperError(errors["KRLParseError"].get("msg"), errors["KRLParseError"].get("status"))
-
-                            self.krl["krl_key_hash"].append(section_data[section_ptr:section_ptr+string_size])
-                            app.logger.debug("Key Hash: " + section_data[section_ptr:section_ptr+string_size].hex())
-                            section_ptr += string_size
                         app.logger.debug("KRL Key Hash Size: " + str(len(self.krl["krl_key_hash"])))
         except OSError as e:
             app.logger.error("OS error: " + str(e))
             raise KeyperError(errors["OSError"].get("msg"), errors["OSError"].get("status"))
 
         app.logger.debug("Exit")
+
+    def read_string_from_buf(self, buf, ptr):
+        ''' Returns a string from buffer '''
+        app.logger.debug("Enter")
+
+        result_string = None
+        result_ptr = ptr
+        buf_len = len(buf)
+
+        string_size = struct.unpack('>i', buf[result_ptr:result_ptr+4])[0]
+        result_ptr += 4
+
+        if (buf_len < result_ptr + string_size):
+            app.logger.error("KRL Parse Error at section. PTR: " + str(result_ptr))
+            raise KeyperError(errors["KRLParseError"].get("msg"), errors["KRLParseError"].get("status"))
+
+        result_string = buf[result_ptr:result_ptr+string_size]
+        result_ptr += string_size
+
+        app.logger.debug("Exit")
+        return result_ptr, result_string
 
     def is_key_revoked(self, key_hash):
         ''' Checks if key hash in KRL '''
